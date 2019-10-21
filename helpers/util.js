04d7028e10d63858665dcbc858ebd0170019ab54
safarudinyoga <safarudinyoga@gmail.com>
2019-10-21 21:34:09 +0700

    90%

--- helpers/util.js
+++ helpers/util.js
@@ -1,19 +1,18 @@
 module.exports = {
-    isLoggedIn: (req, res, next) => {
-        if (req.session.user) {
-            next();
-        } else {
-            req.session.latestUrl = req.originalUrl;
-            console.log(req.session.latestUrl);
-            res.redirect('/')
-        }
-    },
-
-    isLoggedOut: (req, res, next) => {
-        if (req.session.user) {
-            res.redirect('/projects')
-        } else {
-            next();
-        }
+  isLoggedIn: (req, res, next) => {
+    if (req.session.user) {
+      next();
+    } else {
+      req.session.latestUrl = req.originalUrl;
+      res.redirect("/");
     }
-}
+  },
+
+  isLoggedOut: (req, res, next) => {
+    if (req.session.user) {
+      res.redirect("/projects");
+    } else {
+      next();
+    }
+  }
+};
